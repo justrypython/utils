9482e9ed64956081b96df8b2fe5737100692ae83
kerzhao <kerzhao@163.com>
2018-01-22 16:12:18 +0800

    update

--- src/overturn_images.py
+++ src/overturn_images.py
@@ -28,21 +28,25 @@ def main():
         y, x = img.shape[:2]
         overturn_func = overturn_lr(x)
         img = img[:, ::-1]
-        cv2.imwrite(dst_path+'lr/'+i, img)
-        f = open(path+i.replace('.jpg', '.txt'))
-        lines = f.readlines()
-        f.close()
-        lines = [j.replace('\n', '').replace('\r', '').split(',') for j in lines]
-        lines = np.array(lines).astype(np.int)
-        polys = lines[:, :8]
-        labels = lines[:, -1:]
-        polys = polys.reshape([-1, 4, 2])
-        polys = np.apply_along_axis(overturn_func, -1, polys)
-        polys = polys[:, [1, 0, 3, 2]]
-        polys = polys.reshape((-1, 8))
-        polys = np.concatenate([polys, labels], axis=-1)
-        polys = polys.astype(np.str)
-        polys = '\n'.join([','.join(j) for j in polys])
+        try:
+            f = open(path+i.replace('.jpg', '.txt'))
+            lines = f.readlines()
+            f.close()
+            lines = [j.replace('\n', '').replace('\r', '').split(',') for j in lines]
+            lines = np.array(lines).astype(np.int)
+            polys = lines[:, :8]
+            labels = lines[:, -1:]
+            polys = polys.reshape([-1, 4, 2])
+            polys = np.apply_along_axis(overturn_func, -1, polys)
+            polys = polys[:, [1, 0, 3, 2]]
+            polys = polys.reshape((-1, 8))
+            polys = np.concatenate([polys, labels], axis=-1)
+            polys = polys.astype(np.str)
+            polys = '\n'.join([','.join(j) for j in polys])
+        except:
+            print(i)
+            continue
+        cv2.imwrite(dst_path+'lr/lr_'+i, img)
         f = open(dst_path+'lr/lr_'+i.replace('.jpg', '.txt'), 'w')
         f.write(polys)
         f.close()
@@ -52,21 +56,25 @@ def main():
         y, x = img.shape[:2]
         overturn_func = overturn_ud(y)
         img = img[::-1]
-        cv2.imwrite(dst_path+'ud/'+i, img)
-        f = open(path+i.replace('.jpg', '.txt'))
-        lines = f.readlines()
-        f.close()
-        lines = [j.replace('\n', '').replace('\r', '').split(',') for j in lines]
-        lines = np.array(lines).astype(np.int)
-        polys = lines[:, :8]
-        labels = lines[:, -1:]
-        polys = polys.reshape([-1, 4, 2])
-        polys = np.apply_along_axis(overturn_func, -1, polys)
-        polys = polys[:, [3, 2, 1, 0]]
-        polys = polys.reshape((-1, 8))
-        polys = np.concatenate([polys, labels], axis=-1)
-        polys = polys.astype(np.str)
-        polys = '\n'.join([','.join(j) for j in polys])
+        try:
+            f = open(path+i.replace('.jpg', '.txt'))
+            lines = f.readlines()
+            f.close()
+            lines = [j.replace('\n', '').replace('\r', '').split(',') for j in lines]
+            lines = np.array(lines).astype(np.int)
+            polys = lines[:, :8]
+            labels = lines[:, -1:]
+            polys = polys.reshape([-1, 4, 2])
+            polys = np.apply_along_axis(overturn_func, -1, polys)
+            polys = polys[:, [3, 2, 1, 0]]
+            polys = polys.reshape((-1, 8))
+            polys = np.concatenate([polys, labels], axis=-1)
+            polys = polys.astype(np.str)
+            polys = '\n'.join([','.join(j) for j in polys])
+        except:
+            print(i)
+            continue
+        cv2.imwrite(dst_path+'ud/ud_'+i, img)
         f = open(dst_path+'ud/ud_'+i.replace('.jpg', '.txt'), 'w')
         f.write(polys)
         f.close()
