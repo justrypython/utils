1f5bbd7d5354368bf0efcf8a68cd96593181d9d3
kerzhao <kerzhao@163.com>
2018-03-01 14:42:44 +0800

    update

--- test/mm.py
+++ test/mm.py
@@ -105,14 +105,32 @@ def showmm2_1(ratio, times):
     print('the max diff is at %f, and max diff is %f'%(0.001*(b-1), np.max(result.mean(axis=0)-result.std(axis=0))))
     print('end')
     
-def main():
-    for ratio in np.arange(0.51, 0.6, 0.01):
+def showmm2_2():
+    for ratio in np.arange(0.50, 0.6, 0.01):
         #showmm2_1(ratio, 100)
         #showmm2_1(ratio, 200)
         #showmm2_1(ratio, 400)
         showmm2_1(ratio, 1000)
         showmm2_1(ratio, 2000)
         print('#'*70)
+        
+def showmm2_3():
+    while True:
+        result = []
+        results = np.array(mm2([0.01, 0.02, 0.04, 0.057, 0.08], ratio=0.53, t=2000))
+        result.append(results[-1])
+        result = np.log(np.array(result)/10000) 
+        plt.plot(results[:, 0], 'r')
+        plt.plot(results[:, 1], 'b')
+        plt.plot(results[:, 2], 'g')
+        plt.plot(results[:, 3], 'y')
+        plt.plot(results[:, 4], 'm')
+        plt.show()
+        print(result)
+        
+def main():
+    #showmm2_2()
+    showmm2_2()
 
 if __name__ == '__main__':
     main()
