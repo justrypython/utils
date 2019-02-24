0f0723fb614b0eb6f8a41ad89c369848cb23159e
justttry <kerzhao@163.com>
2019-02-24 11:35:22 +0800

    update

---
+++ src/send.py
@@ -0,0 +1,14 @@
+#encoding:UTF-8
+
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello world!')
+
+print(' [x] sent "hello world"')
